3eb600ccd3ba53dfcec4942e2e7d047aecc3fe5d
tparin <ptr@factory-talk.com>
2024-03-01 15:26:35 +0700

    refactor: too much api call on context

--- src/contexts/LoadingContext.tsx
+++ src/contexts/LoadingContext.tsx
@@ -10,16 +10,20 @@ export const LoadingContext = createContext<LoadingContextType>(null!);
 
 export const LoadingProvider = ({ children }: { children: ReactNode }) => {
   const [isLoading, setIsLoading] = useState(false);
+
   const startLoading = () => {
     setIsLoading(true);
   };
+
   const stopLoading = () => {
     setIsLoading(false);
   };
+
   const loading = useMemo(
     () => ({ isLoading, startLoading, stopLoading }),
     [isLoading]
   );
+
   return (
     <LoadingContext.Provider value={loading}>
       {children}
--- src/contexts/UserInfoContext.tsx
+++ src/contexts/UserInfoContext.tsx
@@ -1,24 +1,32 @@
 import { auth } from '@/firebase/config';
 import { getUser } from '@/firebase/users';
 import { IUser } from '@/models/user.model';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useRouter } from 'next/router';
-import { ReactNode, createContext, useEffect, useState } from 'react';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { ReactNode, createContext, useMemo, useState } from 'react';
 
 export const UserInfoContext = createContext<IUser>({});
 
 export const UserInfoProvider = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
+  const [loginUser, setLoginUser] = useState<User | null>(null);
   const [userInfo, setUserInfo] = useState({});
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
+
+  useMemo(() => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        const data = await getUser(user.uid);
+        setLoginUser(user);
+      } else {
+        setLoginUser(null);
+      }
+    });
+
+    const setInfo = async () => {
+      if (loginUser) {
+        const data = await getUser(loginUser.uid);
         if (data) {
           setUserInfo({
-            uid: user.uid,
-            email: user.email,
-            displayName: user.displayName,
+            uid: loginUser.uid,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
             firstName: data.firstName,
             lastName: data.lastName,
             role: data.role,
@@ -27,8 +35,11 @@ export const UserInfoProvider = ({ children }: { children: ReactNode }) => {
       } else {
         setUserInfo({});
       }
-    });
-  }, []);
+    };
+
+    setInfo();
+  }, [loginUser]);
+
   return (
     <UserInfoContext.Provider value={userInfo}>
       {children}
