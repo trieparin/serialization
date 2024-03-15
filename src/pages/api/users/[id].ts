312bccf95ca99236628f22b0fabdb325aecf30ba
tparin <ptr@factory-talk.com>
2024-03-15 21:40:46 +0700

    refactor: api

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -5,57 +5,45 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const users = db.collection('/users');
-  switch (req.method) {
-    case 'GET':
-      try {
-        const { id } = req.query;
-        const doc = await users.doc(id as string).get();
-        res.status(200).json({ data: doc.exists && doc.data() });
-      } catch (e) {
-        res.status(500).json({});
-      }
-      break;
-    case 'PUT':
-      try {
-        const { id } = req.query;
-        const password = req.body;
-        await admin.updateUser(id as string, password);
+  try {
+    await admin.verifyIdToken(req.cookies.token!);
+    const users = db.collection('/users');
+    const { id } = req.query;
+    switch (req.method) {
+      case 'GET':
+        {
+          const doc = await users.doc(id as string).get();
+          res.status(200).json({ data: doc.exists && doc.data() });
+        }
+        break;
+      case 'PUT':
+        await admin.updateUser(id as string, req.body);
         res.status(200).json({ message: 'Change password successfully' });
-      } catch (e) {
-        res.status(500).json({});
-      }
-      break;
-    case 'PATCH':
-      try {
-        const { id } = req.query;
-        const data = req.body;
-        await users.doc(id as string).update(data);
-        if (data.displayName) {
+        break;
+      case 'PATCH':
+        await users.doc(id as string).update(req.body);
+        if (req.body.displayName) {
           await admin.updateUser(id as string, {
-            displayName: data.displayName,
+            displayName: req.body.displayName,
           });
         }
-        if (data.role) {
-          await admin.setCustomUserClaims(id as string, { role: data.role });
+        if (req.body.role) {
+          await admin.setCustomUserClaims(id as string, {
+            role: req.body.role,
+          });
         }
         res.status(200).json({ message: 'Update user info successfully' });
-      } catch (e) {
-        res.status(500).json({});
-      }
-      break;
-    case 'DELETE':
-      try {
-        const { id } = req.query;
+        break;
+      case 'DELETE':
         await admin.deleteUser(id as string);
         await users.doc(id as string).delete();
         res.status(200).json({ message: 'Delete user successfully' });
-      } catch (e) {
-        res.status(500).json({});
-      }
-      break;
-    default:
-      res.status(400).json({});
-      break;
+        break;
+      default:
+        res.status(400).json({});
+        break;
+    }
+  } catch (e) {
+    res.status(500).json({});
   }
 }
