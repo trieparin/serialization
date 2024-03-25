bed371cb23234c822e177c452ade27b1e8bcfb94
tparin <ptr@factory-talk.com>
2024-03-25 14:59:38 +0700

    refactor: adjust code line

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -123,12 +123,9 @@ export async function getServerSideProps({ req }: GetServerSidePropsContext) {
 
     const data: IUser[] = [];
     const snapshot = await db.collection('users').get();
-    snapshot.forEach((doc) =>
-      data.push({
-        uid: doc.id,
-        ...doc.data(),
-      })
-    );
+    snapshot.forEach((doc) => {
+      data.push({ uid: doc.id, ...doc.data() });
+    });
 
     return {
       props: {
