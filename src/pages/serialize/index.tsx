b415dd704b0eae191642d61c08d6b4f74cae472a
tparin <ptr@factory-talk.com>
2024-03-25 21:52:50 +0700

    feat: add serialize table

--- src/pages/serialize/index.tsx
+++ src/pages/serialize/index.tsx
@@ -1,13 +1,62 @@
 import { PageTitle } from '@/components';
-import { admin } from '@/firebase/admin';
+import { admin, db } from '@/firebase/admin';
 import { BaseLayout } from '@/layouts';
+import { ISerialize, SerializeStatus } from '@/models/serialize.model';
 import { Role } from '@/models/user.model';
+import {
+  Badge,
+  IconButton,
+  LabelIcon,
+  Pane,
+  Table,
+  majorScale,
+} from 'evergreen-ui';
 import { GetServerSidePropsContext } from 'next';
 
-export default function SerializePage() {
+export default function SerializePage({ data }: { data: ISerialize[] }) {
+  const renderStatus = (status: string) => {
+    switch (status) {
+      case SerializeStatus.LABELED:
+        return <Badge color="orange">{status}</Badge>;
+      case SerializeStatus.VERIFIED:
+        return <Badge color="purple">{status}</Badge>;
+      default:
+        return <Badge color="teal">{status}</Badge>;
+    }
+  };
+
   return (
     <BaseLayout>
       <PageTitle title="All Serials" />
+      <Pane overflowX="auto">
+        <Table minWidth="max-content">
+          <Table.Head minWidth={1214} paddingRight={0}>
+            <Table.TextHeaderCell>No.</Table.TextHeaderCell>
+            <Table.TextHeaderCell>Label</Table.TextHeaderCell>
+            <Table.TextHeaderCell>Status</Table.TextHeaderCell>
+            <Table.TextHeaderCell>Actions</Table.TextHeaderCell>
+          </Table.Head>
+          <Table.Body>
+            {data.map(({ id, label, status }, index) => (
+              <Table.Row key={id}>
+                <Table.TextCell>{index + 1}</Table.TextCell>
+                <Table.TextCell>{label}</Table.TextCell>
+                <Table.TextCell>{renderStatus(status)}</Table.TextCell>
+                <Table.Cell>
+                  <Pane display="flex" columnGap={majorScale(1)}>
+                    <IconButton
+                      type="button"
+                      name="info"
+                      title="info"
+                      icon={LabelIcon}
+                    />
+                  </Pane>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </Pane>
     </BaseLayout>
   );
 }
@@ -19,8 +68,17 @@ export async function getServerSideProps({ req }: GetServerSidePropsContext) {
     if (role === Role.ADMIN) {
       return { redirect: { destination: '/no-permission' } };
     }
+
+    const data: ISerialize[] = [];
+    const snapshot = await db.collection('serials').get();
+    snapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...(doc.data() as ISerialize) });
+    });
+
     return {
-      props: {},
+      props: {
+        data,
+      },
     };
   } catch (e) {
     return { redirect: { destination: '/' } };
