4126ed745524294c6884bc59e62e568903d7452b
tparin <ptr@factory-talk.com>
2024-04-28 14:40:27 +0700

    feat: add inventory api

---
+++ src/pages/api/items/filter.ts
@@ -0,0 +1,87 @@
+import { admin, db } from '@/firebase/admin';
+import { PageSize } from '@/models/form.model';
+import { IItem } from '@/models/inventory.model';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await admin.verifyIdToken(req.cookies.token!);
+    const items = db.collection('items');
+    if (req.method === 'GET') {
+      const { name, type, offset } = req.query;
+      const data: IItem[] = [];
+      if (name && type) {
+        if (offset) {
+          const snapshot = await items
+            .where('type', '==', type)
+            .where('name', '>=', name)
+            .where('name', '<=', `${name}~`)
+            .limit(PageSize.PER_PAGE)
+            .offset(parseInt(offset as string))
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        } else {
+          const snapshot = await items
+            .where('type', '==', type)
+            .where('name', '>=', name)
+            .where('name', '<=', `${name}~`)
+            .limit(PageSize.PER_PAGE)
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        }
+      } else if (name) {
+        if (offset) {
+          const snapshot = await items
+            .where('name', '>=', name)
+            .where('name', '<=', `${name}~`)
+            .limit(PageSize.PER_PAGE)
+            .offset(parseInt(offset as string))
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        } else {
+          const snapshot = await items
+            .where('name', '>=', name)
+            .where('name', '<=', `${name}~`)
+            .limit(PageSize.PER_PAGE)
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        }
+      } else {
+        if (offset) {
+          const snapshot = await items
+            .where('type', '==', type)
+            .limit(PageSize.PER_PAGE)
+            .offset(parseInt(offset as string))
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        } else {
+          const snapshot = await items
+            .where('type', '==', type)
+            .limit(PageSize.PER_PAGE)
+            .get();
+          snapshot.forEach((doc) => {
+            data.push({ id: doc.id, ...(doc.data() as IItem) });
+          });
+        }
+      }
+      res.status(200).json({ data });
+    } else {
+      res.status(400).json({});
+    }
+  } catch (e) {
+    res.status(401).json({});
+  }
+}
---
+++ src/pages/api/items/index.ts
@@ -0,0 +1,46 @@
+import { admin, db } from '@/firebase/admin';
+import { PageSize } from '@/models/form.model';
+import { IItem } from '@/models/inventory.model';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await admin.verifyIdToken(req.cookies.token!);
+    const items = db.collection('items');
+    if (req.method === 'GET') {
+      const data: IItem[] = [];
+      const { offset } = req.query;
+      const amount = await items.orderBy('type').count().get();
+      const total = Math.ceil(amount.data().count / PageSize.PER_PAGE);
+      if (offset) {
+        const snapshot = await items
+          .orderBy('type')
+          .limit(PageSize.PER_PAGE)
+          .offset(parseInt(offset as string))
+          .get();
+        snapshot.forEach((doc) => {
+          data.push({ id: doc.id, ...(doc.data() as IItem) });
+        });
+      } else {
+        const snapshot = await items
+          .orderBy('type')
+          .limit(PageSize.PER_PAGE)
+          .get();
+        snapshot.forEach((doc) => {
+          data.push({ id: doc.id, ...(doc.data() as IItem) });
+        });
+      }
+      res.status(200).json({ data, total });
+    } else if (req.method === 'POST') {
+      await items.add(req.body);
+      res.status(201).json({ message: 'Create new item successfully' });
+    } else {
+      res.status(400).json({});
+    }
+  } catch (e) {
+    res.status(401).json({});
+  }
+}
