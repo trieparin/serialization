b2b149438194da41f75a3d7349d1ab6454297738
ptr <ptr@factory-talk.com>
2024-09-08 10:09:36 +0700

    refactor: distribute model

--- src/models/distribute.model.ts
+++ src/models/distribute.model.ts
@@ -1,4 +1,22 @@
-export interface IDistribute {
-  stock: string[];
-  shipped: string[];
+import { IProduct } from '@/models/product.model';
+import { ISerialize } from '@/models/serialize.model';
+
+export enum ROLE {
+  MANUFACTURER,
+  DISTRIBUTOR,
+  PHARMACY,
+}
+
+export interface IDistributeInfo {
+  address: string;
+  company: string;
+  serials: string[];
+  role: ROLE;
+}
+
+export interface IDistributeContract {
+  contract?: string;
+  product: IProduct;
+  serialize: ISerialize;
+  distributes: IDistributeInfo[];
 }
