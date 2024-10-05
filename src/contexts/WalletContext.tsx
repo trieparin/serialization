2cba88882771b2502e9977e76260afa290d8591f
Parin T. <parin.trie@gmail.com>
2024-10-05 10:07:48 +0700

    refactor: add local wallet and helper

--- src/contexts/WalletContext.tsx
+++ src/contexts/WalletContext.tsx
@@ -1,10 +1,45 @@
-import { BrowserProvider, Provider } from 'ethers';
-import { ReactNode, createContext, useEffect, useState } from 'react';
+import { BrowserProvider, JsonRpcApiProvider, JsonRpcProvider } from 'ethers';
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-export const WalletContext = createContext<Provider | undefined>(undefined);
+interface IWalletProvider {
+  provider: BrowserProvider | JsonRpcApiProvider | undefined;
+  setMode: Dispatch<SetStateAction<string | undefined>>;
+}
+
+export const WalletContext = createContext<IWalletProvider>(null!);
 
 export const WalletProvider = ({ children }: { children: ReactNode }) => {
-  const [provider, setProvider] = useState<Provider>();
+  const [provider, setProvider] = useState<
+    BrowserProvider | JsonRpcApiProvider | undefined
+  >(undefined);
+  const [mode, setMode] = useState<string | undefined>(
+    process.env.NEXT_PUBLIC_WALLET_MODE
+  );
+
+  const wallet = useMemo(() => {
+    console.log('memo');
+    if (mode === 'test') {
+      const localProvider = new JsonRpcProvider('http://127.0.0.1:8545');
+      setProvider(localProvider);
+    } else {
+      if (typeof window !== 'undefined') {
+        const provider = new BrowserProvider(window.ethereum);
+        setProvider(provider);
+      }
+    }
+    return {
+      provider,
+      setMode,
+    };
+  }, [mode, provider]);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -14,6 +49,6 @@ export const WalletProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <WalletContext.Provider value={provider}>{children}</WalletContext.Provider>
+    <WalletContext.Provider value={wallet}>{children}</WalletContext.Provider>
   );
 };
