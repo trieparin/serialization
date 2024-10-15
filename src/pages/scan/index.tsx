692437630c24e852a9824ac46f3cdb2fe61c8857
Parin T. <parin.trie@gmail.com>
2024-10-15 09:34:31 +0700

    feat: add scan qr

--- src/pages/scan/index.tsx
+++ src/pages/scan/index.tsx
@@ -1,10 +1,21 @@
 import { UserContext } from '@/contexts/UserContext';
 import { auth } from '@/firebase/config';
 import { setCookie } from '@/helpers/cookie.helper';
+import customFetch from '@/helpers/fetch.helper';
 import { checkWallet, connectWallet } from '@/helpers/wallet.helpers';
 import { BlankLayout } from '@/layouts';
-import { Scanner } from '@yudiel/react-qr-scanner';
-import { Card, Heading, majorScale, Pane, Paragraph } from 'evergreen-ui';
+import { IDistribute, ROLE } from '@/models/distribute.model';
+import Traceability from '@/Traceability.json';
+import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner';
+import { Contract } from 'ethers';
+import {
+  Card,
+  Heading,
+  majorScale,
+  Pane,
+  Paragraph,
+  toaster,
+} from 'evergreen-ui';
 import { signInAnonymously } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
@@ -12,6 +23,47 @@ import { useContext, useEffect } from 'react';
 export default function ScanPage() {
   const router = useRouter();
   const { token } = useContext(UserContext);
+
+  const onScan = async (detected: IDetectedBarcode) => {
+    try {
+      const id = detected.rawValue;
+      const fch = customFetch();
+      const { data }: { data: IDistribute } = await fch.get(
+        `/distributes/${id}`
+      );
+      console.log(data);
+      const provider = await connectWallet();
+      let signer;
+      if (checkWallet()) {
+        signer = await provider.getSigner(0);
+      } else {
+        const idx = parseInt(prompt('Input test account index')!);
+        signer = await provider.getSigner(idx);
+      }
+      const contract = new Contract(data.contract, Traceability.abi, signer);
+      const role = parseInt(await contract.checkRole());
+      switch (role) {
+        case ROLE.DISTRIBUTOR:
+          if (data.catalogs[signer.address]) {
+            router.push(`/distribute/request/${id}?address=${signer.address}`);
+          } else {
+            router.push(`/distribute/confirm/${id}?address=${signer.address}`);
+          }
+          break;
+        case ROLE.PHARMACY:
+          if (data.catalogs[signer.address]) {
+            router.push(`/distribute/print/${id}?address=${signer.address}`);
+          } else {
+            router.push(`/distribute/confirm/${id}?address=${signer.address}`);
+          }
+          break;
+        default:
+          router.push(`/distribute/info/${id}?address=${signer.address}`);
+      }
+    } catch (error) {
+      toaster.danger('An error occurred');
+    }
+  };
 
   useEffect(() => {
     if (!token) {
@@ -44,22 +96,9 @@ export default function ScanPage() {
         <Pane
           width="30%"
           marginX="auto"
-          marginBottom={majorScale(3)}
+          style={{ aspectRatio: 1 }}
         >
-          <Scanner
-            onScan={async (detected) => {
-              const id = detected[0].rawValue;
-              const provider = await connectWallet();
-              let signer;
-              if (checkWallet()) {
-                signer = await provider.getSigner(0);
-              } else {
-                const idx = parseInt(prompt('Input test account index')!);
-                signer = await provider.getSigner(idx);
-              }
-              router.push(`/distribute/info/${id}?address=${signer.address}`);
-            }}
-          />
+          <Scanner onScan={(detected) => onScan(detected[0])} />
         </Pane>
       </Card>
     </BlankLayout>
